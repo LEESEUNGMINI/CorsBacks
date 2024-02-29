038bdf02380105c3c176d76ddf765a5a8c02baea
LEESEUNGMINI <dltmdals7295@naver.com>
2024-02-29 10:19:52 +0900

    re

--- index.js
+++ index.js
@@ -8,26 +8,9 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.get("/sound/:name", (req, res) => {
-  const { name } = req.params;
-  if (name == "dog") {
-    res.json({ sound: "멍멍" });
-  } else if (name == "cat") {
-    res.json({ sound: "야옹" });
-  } else if (name == "fig") {
-    res.json({ sound: "꿀꿀" });
-  } else {
-    res.json({ sound: "알수없음" });
-  }
-});
-
-app.get("/cat", (req, res) => {
-  res.send({ name: "고양이" });
-});
-
 app.get("/neople", (req, res) => {
   fetch(
-    "https://api.neople.co.kr/df/jobs?apikey=Mw0fYJwiAJL76wBNq1mezxFH01NMosOZ"
+    `https://c.q-net.or.kr/openapi/Ncs1info/ncsinfo.do?type=json&pageNo=1&numOfRows=10&ServiceKey=GfEBGZl/WVWZaww3GJjoLpRrE++wLFKbom/Sth6vxaX+A8qKV6FBHeMZe0zjSNjGSizunxh5Ylj4ZNegI83s9w==`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -39,7 +22,7 @@ app.get("/neople", (req, res) => {
     });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
 });
